e63eb6e1bd8631952e71b573125119e4fc001cc9
Robin Johnson <robin@robins-imac.home>
2019-02-27 15:54:22 +0000

    Adds ContactList component

---
+++ src/ContactList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactList extends Component {
+  render () {
+    return (
+      <div className='ContactListMain'>
+        Contacts
+      </div>
+    );
+  }
+}
+
+export default ContactList;
