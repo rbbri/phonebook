a69fa5acd6f25f5f5761cf248315c5d7f7ef0741
Robin Johnson <robin@robins-imac.home>
2019-02-27 18:36:15 +0000

    Displays contacts from mock data

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Contact(props) {
-  return <div>Contact</div>
+  return (
+    <div>
+      <h1> {props.name} </h1>
+      <p> {props.phoneNumber} </p>
+      <p> {props.address} </p>
+    </div>
+  )
 }
 
 export default Contact;
