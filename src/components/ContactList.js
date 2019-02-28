f4d361d8e96cff39ebc7c3c00c44898cf55cc36f
rbbri <robin.s.johnson@gmail.com>
2019-02-28 12:39:57 +0000

    Allows searching by contact name

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,7 +5,8 @@ const API = 'http://www.mocky.io/v2/581335f71000004204abaf83'
 
 class ContactList extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    search: ""
   }
 
   componentDidMount(){
@@ -14,17 +15,38 @@ class ContactList extends Component {
       .then(data => this.setState({contacts: data.contacts}));
 }
 
-  render () {
+  changeSearch = e => {
+    this.setState({search: e.target.value})
+  }
+
+
+
+  render() {
+
+    const {search} = this.state;
+    const contacts = this.state.contacts;
+    const filteredContacts = contacts.filter(c => {
+      return c.name.toLowerCase().indexOf( search.toLowerCase() ) !== -1
+    })
+
     return (
       <div className='ContactListMain'>
         Contacts
-        {this.state.contacts.map(c =>
-          <Contact
-            name={c.name}
-            phoneNumber={c.phone_number}
-            address={c.address}
+        <div className='ContactsSearch'>
+          <input type='text'
+                 placeholder='Search'
+                 onChange={this.changeSearch}
           />
-          )}
+        </div>
+        <div className='Contacts'>
+          {filteredContacts.map(c =>
+            <Contact
+              name={c.name}
+              phoneNumber={c.phone_number}
+              address={c.address}
+            />
+            )}
+        </div>
       </div>
     );
   }
