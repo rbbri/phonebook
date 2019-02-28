3d39d9ebfdd86fdfce94a04d922d7e54b2aef1cd
rbbri <robin.s.johnson@gmail.com>
2019-02-28 23:03:22 +0000

    Allows sorting by contact name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact(props) {
   return (
     <div>
-      <h1> {props.name} </h1>
+      <h2> {props.name} </h2>
       <p> Phone: {props.phoneNumber} </p>
       <p> Address: {props.address} </p>
     </div>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -6,40 +6,75 @@ const API = 'http://www.mocky.io/v2/581335f71000004204abaf83'
 class ContactList extends Component {
   state = {
     contacts: [],
-    search: ""
+    filteredContacts: [],
+    search: '',
+    select: "Sort"
   }
 
   componentDidMount(){
-  fetch(API)
-      .then(response => response.json())
-      .then(data => this.setState({contacts: data.contacts}));
-}
+    fetch(API)
+        .then(response => response.json())
+        .then(data => this.setState(
+          {contacts: data.contacts, filteredContacts: data.contacts}
+        )
+      )
+    }
 
-  changeSearch = e => {
-    this.setState({search: e.target.value})
+  changeOrder = e => {
+    this.setState({select: e.target.value});
+    if (e.target.value === 'Ascending') {this.sortAscending()}
+    if (e.target.value === 'Descending') {this.sortDescending()}
   }
 
+  changeSearch = e => {
+    this.setState({search: e.target.value});
+    this.searchContacts();
+  }
 
+  sortByName = (x,y) => {
+    return ((x.name === y.name) ? 0 : ((x.name > y.name) ? 1 : -1));
+  }
+
+  sortAscending = () => {
+    let contacts = this.state.contacts;
+    let filteredContacts = contacts.sort(this.sortByName);
+    this.setState({filteredContacts})
+  }
+
+  sortDescending = () => {
+    let contacts = this.state.contacts;
+    let filteredContacts = contacts.sort(this.sortByName).reverse();
+    this.setState({filteredContacts});
+  }
+
+  searchContacts = () => {
+    let search = this.state.search;
+    let contacts = this.state.contacts;
+    let filteredContacts = contacts.filter(c => {
+      return c.name.toLowerCase().indexOf( search.toLowerCase() ) !== -1
+      })
+    this.setState({filteredContacts});
+  }
 
   render() {
-
-    const {search} = this.state;
-    const contacts = this.state.contacts;
-    const filteredContacts = contacts.filter(c => {
-      return c.name.toLowerCase().indexOf( search.toLowerCase() ) !== -1
-    })
-
     return (
       <div className='ContactListMain'>
-        Contacts
+        <h1> Contacts </h1>
         <div className='ContactsSearch'>
           <input type='text'
                  placeholder='Search'
                  onChange={this.changeSearch}
           />
         </div>
+        <div className='ContactsSort'>
+          <select value={this.state.select} onChange={this.changeOrder}>
+            <option value='Sort'>Sort</option>
+            <option value='Ascending'>Ascending</option>
+            <option value='Descending'>Descending</option>
+          </select>
+        </div>
         <div className='Contacts'>
-          {filteredContacts.map(c =>
+          {this.state.filteredContacts.map(c =>
             <Contact
               name={c.name}
               phoneNumber={c.phone_number}
