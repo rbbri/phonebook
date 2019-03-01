9690f8eb0f15b52c38dd9573c3fda722b0d00e95
rbbri <robin.s.johnson@gmail.com>
2019-03-01 17:00:27 +0000

    Adds tests for changeOrder

--- src/components/ContactList.test.js
+++ src/components/ContactList.test.js
@@ -3,15 +3,33 @@ import ContactList from './ContactList.js'
 import {shallow} from 'enzyme'
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
 configure({ adapter: new Adapter() });
 
+describe('contactList container', () => {
+  test('renders without crashing', () => {
+    shallow(<ContactList />);
+  });
 
-
-test('renders without crashing', () => {
-  shallow(<ContactList />);
+  test('renders correctly', () => {
+    let wrapper = shallow(<ContactList />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
 
-test('renders correctly', () => {
-  let wrapper = shallow(<ContactList />);
-  expect(wrapper).toMatchSnapshot();
-});
+describe('#changeOrder', () => {
+  test('it calls sortAscending when ascending is selected', () => {
+    let sortAscending = sinon.stub();
+    let wrapper = shallow(<ContactList />);
+    let dropdown = wrapper.find('select');
+    dropdown.simulate('change', {target: {value: 'Ascending'}})
+    expect(sortAscending).toHaveBeenCalled
+  })
+  test('it calls sortDescending when descending is selected', () => {
+    let sortDescending = sinon.stub();
+    let wrapper = shallow(<ContactList />);
+    let dropdown = wrapper.find('select');
+    dropdown.simulate('change', {target: {value: 'Ascending'}})
+    expect(sortDescending).toHaveBeenCalled
+  })
+})
